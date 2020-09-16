ca6c9aa76b2f45a569f4bed457d9fa38c0461513
Scott <scottdouglas238@gmail.com>
2020-09-16 00:30:51 -0700

    project2 work

--- script.js
+++ script.js
@@ -1,61 +1,34 @@
-$(".btn-search").on("click", function (e) {
-    e.preventDefault();
-    let searchTerm = $("#searchTerm").val();
-    let articleNum = $("#numberOfRecords").val();
-    let inputStartYear = $("#startYear").val();
-    if (inputStartYear === "") {
-        startYear = "";
-    } else {
-        startYear = "&begin_date=" + inputStartYear + "0101"
-    }
-    let inputEndYear = $("#endYear").val();
-    if (inputEndYear === "") {
-        endYear = ""
-    } else {
-        endYear = "&end_date=" + inputEndYear + "1231"
-    }
-    
-    let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + startYear + endYear + "&api-key=ED1VL6lK1MTnpi2Jq1BlwPP5OO6uA6uv"
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response)
-            returnedArticles = response.response.docs.slice(0, articleNum)
-            console.log(returnedArticles)
-            for (let i = 0; i < returnedArticles.length; i++) {
-                let article = returnedArticles[i];
-                titleAjax = article.headline.main; // title
-                authorAjax = article.byline.original; // author
-                dateAjax = article.pub_date; // publish date
-                sectionAjax = article.section_name; // section/category
-                linkAjax = article.web_url; // link to article
+const apiKey1 = "H1WZOIEMOXEW1Z2H" 
+const apiKey2 = "UVV1OIJYR1HSOP3Z";
 
+$("#submit").on("click",  function(){
+  let companySearch;
+  companySearch = $("#input").val();
+$.ajax({
+    url: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + companySearch + "&apikey=" + apiKey1,
+    method: "GET"
+}).then(function(response){
+  for (let i = 0; i < 2; i++) {
+    let compSymbol = response.bestMatches[i]["1. symbol"];
 
-                var card = $("<div>")
-                card.attr("class", "card")
-                var newResult = $("<div>");
-                newResult.attr("id", "new-result")
-                newResult.attr("class", "card-body")
-                //create the card
-                //create various elements of the card
-                var headline = $("<h3>");
-                headline.attr("class", "card-title").text(titleAjax)
-                var author = $("<h3>");
-                author.attr("class", "card-subtitle").text(authorAjax)
-                var section = $("<h3>");
-                section.attr("class", "card-text").text(sectionAjax)
-                var date = $("<h3>")
-                date.attr("class", "card-text").text(dateAjax)
-                var link = $("<a>")
-                link.attr("href", "#")
-                link.attr("class", "card-link").text(linkAjax)
-                //append to the card
-                newResult.append(headline, author, section, date, link)
-                $("#results").append(card)
-                card.append(newResult)
-            // }
-        }
-        )
+  $.ajax({
+    url: "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + compSymbol +"&apikey=" + apiKey2,
+    method: "GET"
+  }).then(function(response){
+    let list = [];
+    const companyName = Object.values(response)
+    const companyNames = companyName[2];
+    list.push(companyNames);
+    // console.log(list)
+    for (let i = 0; i < list.length; i++) {
+      const element = list[i];
+      console.log(element)
+      const button = $("<button>").attr("class", "btn btn-dark compButton").append(element)
+      $("#companyNameButton").attr("display", "flex");
+      $("#companyNameButton").append(button);
+    }
+  })
+
+  }
+})
 })
